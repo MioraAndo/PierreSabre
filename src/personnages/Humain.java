f08719e68b5043807f3f84bd6fb9feeabf93355e
MioraAndo <andoniavorazanadrakoto@gmail.com>
2025-11-14 08:49:37 +0100

    creation de la classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,50 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private int Argentamt;
+	private String Boissonfav;
+	public String getNom() {
+		return nom;
+	}
+	public int getArgentamt() {
+		return Argentamt;
+	}
+	
+	public Humain(String nom, int argentamt, String boissonfav) {
+		this.nom = nom;
+		Argentamt = argentamt;
+		Boissonfav = boissonfav;
+	}
+	protected void parler(String texte) {
+		System.out.println("(" +nom+")"+texte);
+	}
+	public void direBonjour() {
+		parler("Bonjour ! Je m'appelle " + nom + " et j'aime boire du " + Boissonfav);
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de " + Boissonfav + " ! GLOUPS ! ");
+	}
+	
+	public void acheter(String bien, int prix){
+		if(Argentamt>=prix) {
+			parler("j'ai "+ Argentamt + " sous en poche. Je vais m'offrir " + bien + " à " + prix );
+		}
+		else {
+			parler("Je n'ai plus que "+ Argentamt + " sous en poche. Je ne peux même pas m'offrir un"+ bien + " à " + prix + " sous");
+		}
+	}
+	
+	private void gagnerArgent(int gain) {
+		Argentamt+=gain;
+	}
+	
+	private void perdreArgent(int perte) {
+		Argentamt-=perte;
+	}
+	
+	
+	
+	
+}
