142c12a012a2b6bc233e8ab52e7d768b5235ec97
MioraAndo <andoniavorazanadrakoto@gmail.com>
2025-11-18 09:09:28 +0100

    le clan des Yakuzas

--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -34,6 +34,11 @@ public class Yakuza extends Humain {
 	public int getReputation() {
 	    return reputation;
 	}
+	@Override
+	public void direBonjour() {
+		super.direBonjour();
+		parler("Mon clan est celui de " + clan);
+	}
 	
 	
 }
